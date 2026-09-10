4f952642812cf921577480d2f0b7dbeef2c702dd
Mohammadreza <mohammadreza.so2004@gmail.com>
2026-09-10 12:50:14 +0330

    connect cleaning functions to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from processing.clean import clean_price, clean_stock
 
 response = requests.get('https://game-center.ir/product/resident-evil-requiem/', headers={'User-Agent': 'Mozilla/5.0'}, timeout=30)
 soup = BeautifulSoup(response.text, 'html.parser')
 title = soup.find('h1', class_='product_title')
 name = title.get_text(strip=True)
-print("Name:", name)
 
 price_container =  soup.find('p', class_='price')
 prices = price_container.find_all('span', class_='woocommerce-Price-amount')
+
+price_numbers = []
 for price in prices:
-    print("Price:", price.get_text(strip=True))
+    price_numbers.append(clean_price(price.get_text()))
+
+final_price = min(price_numbers)
 
 stock_meta = soup.find('meta', attrs={'name':'twitter:data2'})
 stock = stock_meta['content']
-print("stock:", stock)
+final_stock = clean_stock(stock)
+
+print("Name:", name)
+print("Price:", final_price)
+print("Stock:", final_stock)
