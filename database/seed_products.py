fc7bb925459aa7a007c1804ed9badd3862a3ff83
Mohammadreza <mohammadreza.so2004@gmail.com>
2026-09-13 14:19:47 +0330

    add loop over all active products

--- database/seed_products.py
+++ database/seed_products.py
@@ -3,7 +3,8 @@ import sqlite3
 connection = sqlite3.connect("price_tracker.db")
 connection.execute(
     "INSERT INTO products (name, platform, store, url) VALUES (?, ?, ?, ?)",
-    ("اکانت Resident Evil Requiem", "PS5", "game-center", "https://game-center.ir/product/resident-evil-requiem/")
+    ("اکانت بازی Tekken 8 Advanced Edition", "PS5", "game-center",
+ "https://game-center.ir/product/%d8%a7%da%a9%d8%a7%d9%86%d8%aa-%d8%a8%d8%a7%d8%b2%db%8c-tekken-8-advanced-edition/")
 )
 
 connection.commit()
--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from scraper.scraper import scrape_product
 
 connection = sqlite3.connect("database/price_tracker.db")
 
-product = connection.execute("SELECT id, url FROM products WHERE is_active = 1").fetchone()
-product_id = product[0]
-url = product[1]
+products = connection.execute("SELECT id, url FROM products WHERE is_active = 1").fetchall()
 
-data = scrape_product(url)
-
-connection.execute(
-    "INSERT INTO price_history (product_id, price, in_stock) VALUES (?, ?, ?)",
-    (product_id, data["price"], data["in_stock"])
-)
+for product in products:
+    product_id = product[0]
+    url = product[1]
+    data = scrape_product(url)
+    connection.execute(
+        "INSERT INTO price_history (product_id, price, in_stock) VALUES (?, ?, ?)",
+        (product_id, data["price"], data["in_stock"])
+    )
+    print("Saved:", data)
 
 connection.commit()
 connection.close()
-print("Saved:", data)
